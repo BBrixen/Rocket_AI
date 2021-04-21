fixed z angle calculation, graphics are slightly misleading. y velocity only accounted for in x velocity graph

rocket_versions/rocketv4/physics.py
@@ -7,8 +7,8 @@ from perlin_noise import PerlinNoiseFactory
 # if we want meters per second we must define that 1 frame is 1 second
 FORCE_FROM_THRUST = 800  # how much the acceleration increases from thrust, measured in N
 THRUST_PER_UNIT_FUEL = 1  # how much thrust from 1 unit of fuel
-TILT_RATIO = 0  # how much the rocket tilts in frame
-FORCE_FROM_WIND = 1  # multiplied by wind (value from 0-100), measured in N
+TILT_RATIO = 0.5  # how much the rocket tilts in frame
+FORCE_FROM_WIND = 100  # multiplied by wind (value from 0-1), measured in N
 MASS = 20  # kg
 ACCELERATION_FROM_GRAVITY = -9.8
 FORCE_FROM_GRAVITY = MASS * ACCELERATION_FROM_GRAVITY  # currently -196N
@@ -19,18 +19,20 @@ CROSS_SECTIONAL_AREA = math.pi * RADIUS**2  # this is measured in meters
 AIR_DENSITY = 1.225  # kg/m^3
 
 # -------------------------------classification-------------------------------
-# current max velocity is 136m/s
+# terminal velocity moving straight down is:
+# math.sqrt((-2*FORCE_FROM_GRAVITY) / (AIR_DENSITY*CROSS_SECTIONAL_AREA*DRAG_COEFFICIENT))
+# which is a positive number. if the rocket is moving downwards, its max velocity_y cannot exceed this
 bounds = {'max_acceleration': 100,  # this is also used for preprocessing
-          'max_velocity': math.sqrt((-2*FORCE_FROM_GRAVITY) / (AIR_DENSITY*CROSS_SECTIONAL_AREA*DRAG_COEFFICIENT)),
+          'max_velocity': 300,
           'max_y': 10000,
           'min_y': 0,
           'max_fuel': 10,
           'max_time': 100,
-          'max_x': 10000,
+          'max_x': 100000,
           'min_x': 0,
-          'max_z': 10000,
+          'max_z': 100000,
           'min_z': 0,
-          'max_wind': 100,
+          'max_wind': FORCE_FROM_WIND,
           'min_wind': 0}
 
 all_value_names = ['acceleration', 'velocity', 'y', 'fuel', 'time', 'x', 'x_tilt', 'x_direction', 'z', 'z_tilt',
@@ -47,17 +49,26 @@ angles = ['x_tilt', 'x_direction', 'z_tilt', 'z_direction', 'wind_direction']
 # -------------------------------environment constants-------------------------------
 wind_mags = []
 wind_angles = []
+rt2 = math.sqrt(2)
 
 
 def init_env():
     # i might want to transform the curves to better fit a realistic environment
     global wind_mags, wind_angles
-    mags_noise_generator = PerlinNoiseFactory(1, octaves=2, unbias=True)
-    angle_noise_generator = PerlinNoiseFactory(1, octaves=3, unbias=True)
-    div = 10
+    x_noise_generator = PerlinNoiseFactory(1, octaves=2, unbias=True)
+    z_noise_generator = PerlinNoiseFactory(1, octaves=3, unbias=True)
+    div = 100
     # 50* instead of 100/2*
-    wind_mags = [50*(mags_noise_generator(i/div)+1) for i in range(bounds['max_time'])]
-    wind_angles = [(angle_noise_generator(i/div)+1)/2 for i in range(bounds['max_time'])]
+    wind_x = [(x_noise_generator(i/div))/rt2 for i in range(bounds['max_time'])]
+    wind_z = [(z_noise_generator(i/div))/rt2 for i in range(bounds['max_time'])]
+    wind_mags = []
+    wind_angles = []
+    for i in range(len(wind_x)):
+        magnitude = math.sqrt(wind_x[i]**2 + wind_z[i]**2)
+        angle = math.atan2(wind_z[i], wind_x[i]) / (2*math.pi)
+        angle %= 1
+        wind_mags.append(magnitude)
+        wind_angles.append(angle)
 
 
 def apply_physics(state, actions):
@@ -99,8 +110,8 @@ def apply_physics(state, actions):
     force_x_component = force_xy_projection * math.cos(force_angle_x_projection)
 
     # calculating wind components
-    wind_z_component = math.cos(wind_angle)
-    wind_x_component = math.sin(wind_angle)
+    wind_z_component = math.sin(wind_angle)
+    wind_x_component = math.cos(wind_angle)
     wind_z = FORCE_FROM_WIND * state['wind'] * wind_z_component
     wind_x = FORCE_FROM_WIND * state['wind'] * wind_x_component
 
@@ -121,7 +132,6 @@ def apply_physics(state, actions):
 
     # calculating and storing new magnitude
     cur_acceleration = math.sqrt((acceleration_x ** 2 + acceleration_y ** 2 + acceleration_z ** 2))
-    cur_acceleration *= np.sign(acceleration_y)
     cur_acceleration = round(cur_acceleration, 10)
     state['acceleration'] = cur_acceleration
 
@@ -150,17 +160,21 @@ def apply_physics(state, actions):
 
     # calculating and storing new velocity magnitude
     cur_velocity = math.sqrt((velocity_x ** 2 + velocity_y ** 2 + velocity_z ** 2))
-    cur_velocity *= np.sign(velocity_y)
     cur_velocity = round(cur_velocity, 10)
     state['velocity'] = cur_velocity
 
     # finding x and z angles
+    print('x:', velocity_x)
+    print('y:', velocity_y)
+    print('z:', velocity_z)
     vel_angle_x = math.atan2(velocity_y, velocity_x) / (2 * math.pi)
     xy_projection_magnitude = math.sqrt(velocity_y ** 2 + velocity_x ** 2)
-    xy_projection_magnitude *= np.sign(velocity_y)
     vel_angle_z = math.atan2(xy_projection_magnitude, velocity_z) / (2 * math.pi)
     state['x_direction'] = vel_angle_x
     state['z_direction'] = vel_angle_z
+    print('r:', xy_projection_magnitude)
+    print('angle x:', str(vel_angle_x * math.pi * 2))
+    print('angle z:', str(vel_angle_z * math.pi * 2))
 
     state['time'] += 1
 